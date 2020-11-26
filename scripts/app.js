2e47f1ddf073d8b7fb6832fa8f3085794ea2d0e6
Amateo642 <KitlyaevDD@mail.ru>
2020-11-26 23:59:30 +0300

    slider

---
+++ scripts/app.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    arrows:true,
+    dots:true,
+    draggable:true,
+    swipe:true,
+    fade:true,
+  });
+
+
+
+  /*$('.slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  fade: true,
+  asNavFor: '.slider-mini'
+  });
+
+  $('.slider-mini').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider',
+  arrows:false,
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true
+  });*/
+});
